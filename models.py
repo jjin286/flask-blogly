6d9251ac2ca8c7836aba5d5db97b9889f115f70c
JaeWon Jin <j1jin286@gmail.com>
2023-10-04 11:53:17 -0700

    Templates, models added

--- models.py
+++ models.py
@@ -1,1 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 """Models for Blogly."""
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """Model for Users"""
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    first_name = db.Column(
+        db.String(50),
+        nullable=False
+    )
+    last_name = db.Column(
+        db.String(50),
+        nullable=False
+    )
+    image_url = db.Column(
+        db.String(200),
+        nullable=True
+    )
+
+
